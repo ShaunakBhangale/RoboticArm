f92b5ca91741285cc859ce619942810154839326
Shaunak Bhangale <shaunakbhangale@gmail.com>
2026-06-27 21:54:14 -0400

    got rid of .gitignore contents

---
+++ .pio/build/esp32dev/FrameworkArduino/USB.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
+ /Users/shaunak/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
+ /Users/shaunak/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /Users/shaunak/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ /Users/shaunak/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ /Users/shaunak/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
