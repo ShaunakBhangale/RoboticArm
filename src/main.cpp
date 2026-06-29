aace7aefebc8a616f19d5ebcbb57d21912d988f5
Shaunak Bhangale <shaunakbhangale@gmail.com>
2026-06-28 21:57:17 -0400

    Small changes

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 // For all, max speed, acceleration, bound, and microstep need to be like tested
 StepperJoint J1(25, 16, 1000, 150, 45, 22, 32);
 StepperJoint J2(25, 16, 500, 150, 45, 25, 26);
-StepperJoint J3(25, 16, 500, 250, 45, 23, 33);
-StepperJoint J4(15, 16, 500, 250, 45, 13, 4);
+StepperJoint J3(25, 16, 1000, 250, 45, 23, 33);
+StepperJoint J4(15, 16, 1000, 500, 45, 13, 4);
 StepperJoint J5(15, 16, 1000, 500, 45, 17, 16);
 
 // bound,pulse
@@ -13,23 +13,24 @@ ServoJoint J6(45, 27);
 ServoJoint gripper(45, 21);
 
 // Positive Directions (J1, J2, J3, J4, J5)
-// CW, CW, CCW, CCW, CW
-bool atHome = true;   // tracks which pose we're in
+// CW, CW, CW, CCW, CW
 
+bool atHome = true;   
 
 void runMove() {
     if (atHome) {
         // J1.moveToAngle(45);
-        J2.moveToAngle(45);
-        J3.moveToAngle(45);
-        J4.moveToAngle(-30);
-        J5.moveToAngle(30);
+        // J2.moveToAngle(45);
+        // J3.moveToAngle(-60);
+        // J3.moveToAngle(30);
+        // J4.moveToAngle(-20);
+        // J5.moveToAngle(90);
     } else {
         // J1.moveToAngle(0);
-        J2.moveToAngle(0);
-        J3.moveToAngle(0);
-        J4.moveToAngle(0);
-        J5.moveToAngle(0);
+        // J2.moveToAngle(0);
+        // J3.moveToAngle(0);
+        // J4.moveToAngle(0);
+        // J5.moveToAngle(0);
     }
     atHome = !atHome;
 }
@@ -56,7 +57,6 @@ void loop() {
             Serial.println(atHome ? "Moving to 0" : "Moving out");
         }
     }
-
     J1.update();
     J2.update();
     J3.update();
